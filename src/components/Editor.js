75545d4ee326d16a32ecc5557d7d0bacc00e5135
nninnnin <nninnnin7@gmail.com>
2024-08-12 13:52:23 +0900

    이미지 핸들러 작성중: memex-fetcher 업데이트 필요

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
 function Editor() {
-  return <ReactQuill />;
+  const ref = useRef();
+
+  useEffect(() => {
+    // Add custom image handler
+    const quill = ref.current.getEditor();
+    const toolbar = quill.getModule("toolbar");
+
+    const handleImage = (e) => {
+      const input = document.createElement("input");
+      input.setAttribute("type", "file");
+      input.setAttribute("accept", "image/*");
+      input.click();
+
+      input.addEventListener("change", (e) => {
+        console.log(e.target.files);
+
+        const file = e.target.files[0];
+
+        // upload image to memex server
+      });
+    };
+
+    toolbar.addHandler("image", handleImage);
+  }, []);
+
+  const handleButtonClick = () => {
+    console.log(ref.current.value);
+  };
+
+  return (
+    <>
+      <ReactQuill
+        modules={{
+          toolbar: ["image"],
+        }}
+        ref={ref}
+      />
+
+      <button onClick={handleButtonClick}>get value</button>
+    </>
+  );
 }
 
 export default Editor;
