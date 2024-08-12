d11429bff1ad8eaac879dfa011554b4386c0d79d
nninnnin <nninnnin7@gmail.com>
2024-08-12 18:29:49 +0900

    WIP: 이미지 핸들러

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -17,12 +17,46 @@ function Editor() {
       input.setAttribute("accept", "image/*");
       input.click();
 
-      input.addEventListener("change", (e) => {
+      input.addEventListener("change", async (e) => {
         console.log(e.target.files);
 
         const file = e.target.files[0];
 
-        // upload image to memex server
+        console.log(file);
+        const arrayBuffer = await file.arrayBuffer();
+
+        const projectId = "71355a64";
+
+        const res = await fetch(
+          `https://api.memexdata.io/memex/api/projects/${projectId}/files/access`,
+          {
+            method: "POST",
+            body: file,
+            headers: {
+              "Access-Token":
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJSRUJFTDkiLCJpYXQiOjE3MjE4OTkxODcsImV4cCI6MjAzNzI1OTE4NywiSUQiOiI3MTM1NWE2NCIsIkRPTUFJTiI6WyIqIl0sIlRZUEUiOiJFWFRFUk5BTCJ9.KyIdem9bfHSjSPPbV2JP1dwTjUIilXfSj4nhzR2uhQg",
+            },
+          }
+        );
+
+        console.log(await res.json());
+
+        // const range = getEditor().getSelection(true);
+
+        // getEditor().insertEmbed(range.index, "image", `/images/spinner.svg`);
+
+        // try {
+        //   // upload image to memex server
+
+        //   // const filePath = `contents/temp/${Date.now()}`;
+        //   // const url = await uploadImage(file, filePath);
+
+        //   getEditor().deleteText(range.index, 1);
+        //   getEditor().insertEmbed(range.index, "image", url);
+        //   getEditor().setSelection(range.index + 1);
+        // } catch (e) {
+        //   getEditor().deleteText(range.index, 1);
+        // }
       });
     };
 
